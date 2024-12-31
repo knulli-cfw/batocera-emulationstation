495cb1c7ce0fd675f4348291389a36fdb5ac24eb
chrizzo-hb <chrizzo@unterderbruecke.de>
2024-12-31 05:12:19 +0100

    Fixed a lot of syntax issues (C++ is a language spoken in hell)

--- es-app/src/guis/knulli/GuiRgbSettings.h
+++ es-app/src/guis/knulli/GuiRgbSettings.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "guis/GuiSettings.h"
+#include "components/SliderComponent.h"
+#include "components/OptionListComponent.h"
+#include "components/SwitchComponent.h"
 
 class GuiRgbSettings : public GuiSettings
 {
 public:
-        GuiRgbSettings(Window* window);
+    GuiRgbSettings(Window* window);
 private:
-        std::shared_ptr<SliderComponent> addSlider(std::string label, float min, float max, float step, std::string unit, std::string description);
-        void setDefaultValueForSlider(std::shared_ptr<SliderComponent> slider, float defaultValue, std::string settingsID);
-        std::shared_ptr<OptionListComponent<std::string>> addOptionList(const std::string& title, const std::vector<std::pair<std::string, std::string>>& values, const std::string& settingsID, bool storeInSettings, const std::function<void()>& onChanged);
-
+    std::shared_ptr<SliderComponent> createSlider(std::string label, float min, float max, float step, std::string unit, std::string description);
+    void setDefaultValueForSlider(std::shared_ptr<SliderComponent> slider, float defaultValue, std::string settingsID);
+    std::shared_ptr<OptionListComponent<std::string>> createSwitch(std::string label, std::string variable, std::string description);
 };
