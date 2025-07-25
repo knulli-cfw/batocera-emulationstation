6418fe1dacc17d343fcdbced7a0daf34927525b3
Benoit S <tartifless@gmail.com>
2025-07-25 17:34:08 +0200

    Fix script update-gamelists

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -2997,15 +2997,15 @@ void GuiMenu::updateGameLists(Window* window, bool confirm)
 
 	if (!confirm)
 	{
-		Scripting::fireEvent("update-gamelists");
 		ViewController::reloadAllGames(window, true, true);
 		return;
 	}
 
 	window->pushGui(new GuiMsgBox(window, _("REALLY UPDATE GAMELISTS?"), _("YES"), [window]
 		{
-		ViewController::reloadAllGames(window, true, true);
-		},
+			Scripting::fireEvent("update-gamelists");
+			ViewController::reloadAllGames(window, true, true);
+		}, 
 		_("NO"), nullptr));
 }
 
